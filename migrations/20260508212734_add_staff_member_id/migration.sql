9fce81c1a59290b7b8b1f62362469b0e602aadac
qwc6gkkn8j-pixel <qwc6gkkn8j@privaterelay.appleid.com>
2026-05-09 12:20:38 +0200

    Delete migrations/20260508212734_add_staff_member_id directory

--- migrations/20260508212734_add_staff_member_id/migration.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add staffMemberId to Appointment
--- Created: 2026-05-08
-
-ALTER TABLE "Appointment" ADD COLUMN IF NOT EXISTS "staffMemberId" TEXT;
-
-CREATE INDEX IF NOT EXISTS "Appointment_staffMemberId_idx" ON "Appointment"("staffMemberId");
